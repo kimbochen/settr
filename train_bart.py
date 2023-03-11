4d57315ac4042408cd470c87a6a1b567e1f217ac
kimbochen <chentenghung@gmail.com>
2023-03-11 02:31:41 +0000

    Converted to Hugging Face Trainer.

--- train_bart.py
+++ train_bart.py
@@ -6,9 +6,12 @@ from pathlib import Path
 from absl import app, flags
 from torch.optim import AdamW
 from transformers import AutoTokenizer, BartForConditionalGeneration
+from transformers import DataCollatorForSeq2Seq
+from transformers import TrainingArguments, Trainer
+from transformers import get_constant_schedule
 from tqdm.auto import tqdm
 
-from data_preprocess import EMO_LIST, build_dataloader_fn
+from data_preprocess import build_dataset
 
 
 os.environ['TRANSFORMERS_NO_ADVISORY_WARNINGS'] = 'true'
@@ -26,35 +29,38 @@ flags.DEFINE_integer('n_epochs', None, 'Number of epochs')
 def main(argv):
     tokenizer = AutoTokenizer.from_pretrained(FLAGS.ckpt)
     model = BartForConditionalGeneration.from_pretrained(FLAGS.ckpt).to('cuda')
-    build_dataloader = build_dataloader_fn(model, tokenizer)
-    train_dl = build_dataloader(data_split='train')
+    optim = AdamW(model.parameters(), lr=FLAGS.lr)
+    sched = get_constant_schedule(optim)
 
-    optimizer = AdamW(model.parameters(), lr=FLAGS.lr)
-    model.train()
+    def compute_metrics(eval_preds):
+        summ_ids, label_ids = eval_preds
+        summ = tokenizer.batch_decode(summ_ids, skip_special_tokens=True)
+        labels = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+        return metric.compute(predictions=summs, references=labels)
 
-    with tqdm(total=FLAGS.n_steps) as pbar:
-        for step, batch in enum_steps(train_dl, FLAGS.n_steps*FLAGS.grad_acc):
-            outputs = model(**batch)
-            loss = outputs.loss
-            loss.backward()
-            if (step + 1) % FLAGS.grad_acc == 0:
-                optimizer.step()
-                optimizer.zero_grad()
-                pbar.set_description(f'Loss: {loss:.4f}')
-                pbar.update(1)
+    train_args = TrainingArguments(
+        f'runs2/bart-base-lr{FLAGS.lr:.2e}-{FLAGS.n_epochs}epochs',
+        evaluation_strategy='epoch',
+        per_device_train_batch_size=FLAGS.batch_size,
+        per_device_eval_batch_size=FLAGS.batch_size,
+        gradient_accumulation_steps=FLAGS.grad_acc,
+        learning_rate=FLAGS.lr,
+        num_train_epochs=FLAGS.n_epochs,
+        logging_strategy='epoch'
+    )
 
-    save_ckpt = f'runs/bart-large-cnn-lr{FLAGS.lr:.2e}-step{FLAGS.n_steps}'
-    tokenizer.save_pretrained(save_ckpt, from_pt=True)
-    model.save_pretrained(save_ckpt, from_pt=True)
+    trainer = Trainer(
+        model=model,
+        args=train_args,
+        data_collator=DataCollatorForSeq2Seq(tokenizer, model, padding='longest'),
+        train_dataset=build_dataset(tokenizer, data_split='train'),
+        eval_dataset=build_dataset(tokenizer, data_split='val'),
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics,
+        optimizers=[optim, sched]
+    )
 
-
-def enum_steps(dl, n_steps):
-    def inf_loop():
-        while True:
-            for batch in dl:
-                batch = {k : v.to('cuda') for k, v in batch.items()}
-                yield batch
-    return zip(range(n_steps), inf_loop())
+    trainer.train()
 
 
 if __name__ == '__main__':
