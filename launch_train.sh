50d3bb42f660a98d92659b7ed34f87317dc3b7c5
kimbochen <chentenghung@gmail.com>
2023-03-12 00:23:13 +0000

    Added new experiment.

--- launch_train.sh
+++ launch_train.sh
@@ -9,4 +9,6 @@ set -o xtrace
 
 # python train_bart.py --lr="1e-6" --ckpt="facebook/bart-large-cnn" --n_steps=300 --batch_size=8 --grad_acc=2
 # python train_bart.py --ckpt="bart-large-cnn-lr1.00e-06-step300" --lr="5e-5" --n_steps=300 --batch_size=8 --grad_acc=2
-python train_bart.py --ckpt="bart-large-cnn-lr5.00e-05-step300" --lr="1e-5" --n_steps=300 --batch_size=8 --grad_acc=2
+# python train_bart.py --ckpt="bart-large-cnn-lr5.00e-05-step300" --lr="1e-5" --n_steps=300 --batch_size=8 --grad_acc=2
+
+python train_bart.py --batch_size=16 --grad_acc=2 --lr="5e-5" --ckpt="facebook/bart-base" --n_epochs=10
