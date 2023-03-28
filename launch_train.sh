57c15c38c368f442f080f4d838e5c31412ab68f6
kimbochen <chentenghung@gmail.com>
2023-03-28 03:05:29 +0000

    Added experiments using new scripts.

--- launch_train.sh
+++ launch_train.sh
@@ -20,5 +20,26 @@ set -o xtrace
 # python train_bart.py --ckpt="facebook/bart-base" --batch_size=16 --grad_acc=2 --lr="2e-5" --warmup=400 --n_steps=800 --eval_freq=40 --save_dir="runs/bart-base-more-warmup"
 
 # python train_bart.py --ckpt="facebook/bart-base" --batch_size=16 --grad_acc=2 --lr="2e-5" --warmup=400 --n_steps=1200 --eval_freq=40 --save_dir="runs/bart-base-balanced-data-train-longer"
-python train_bart.py --ckpt="t5-base" --batch_size=8 --grad_acc=4 --lr="2e-5" --warmup=400 --n_steps=1200 --eval_freq=40 --save_dir="runs/t5-base-balanced-data-train-longer"
+# python train_bart.py --ckpt="t5-base" --batch_size=8 --grad_acc=4 --lr="2e-5" --warmup=400 --n_steps=1200 --eval_freq=40 --save_dir="runs/t5-base-balanced-data-train-longer"
 # python train_bart.py --ckpt="bigscience/T0_3B" --batch_size=1 --grad_acc=32 --lr="2e-5" --warmup=400 --n_steps=1200 --eval_freq=60 --save_dir="runs/t0-3b-train-longer"
+
+
+randomSeeds=(39 85 99 42 27)
+
+# How does balancing dataset affect performance
+for i in {0..4}
+do
+    python train_model.py \
+        --ckpt "facebook/bart-base" --exp_name "allsumm_${i}" \
+        --seed ${randomSeeds[i]} --dd "allsumm_concat" \
+        --train_steps 1200 --lr "2e-5" --warmup 400 \
+        --batch_size 16 --grad_acc 2 \
+        --eval_freq 60 
+
+    python train_model.py \
+        --ckpt "facebook/bart-base" --exp_name "balanced_${i}" \
+        --seed ${randomSeeds[i]} --dd "balanced" \
+        --train_steps 1200 --lr "2e-5" --warmup 400 \
+        --batch_size 16 --grad_acc 2 \
+        --eval_freq 60 
+done
